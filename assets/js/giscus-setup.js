152439c50e1a6c45a3f4a84b431a231723f685a7
hc-nguyen-maths <hc-nguyen-maths@users.noreply.github.com>
2026-04-28 21:22:08 +0000

    Deploying to gh-pages from @ hc-nguyen-maths/hc-nguyen-maths.github.io@2a3cc1509ca56a347e9a15ceae2f7e6a7f5063e9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "hc-nguyen-maths/hnguyen.github.io",
+    "data-repo": "hc-nguyen-maths/hc-nguyen-maths.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
