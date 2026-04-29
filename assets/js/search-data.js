350617873cdd3ea49df92aa0ca0d30b03c606990
hc-nguyen-maths <haichau1998@yahoo.fr>
2026-04-29 00:05:23 +0000

    Deploying to gh-pages from @ hc-nguyen-maths/hc-nguyen-maths.github.io@925b1967b932ca704f579a5f2481bb900a31711e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -58,21 +51,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "news-site-personnel-mis-en-ligne",
+        },{id: "news-site-personnel-mis-en-ligne",
           title: 'Site personnel mis en ligne.',
           description: "",
           section: "News",},{id: "projects-project-1",
