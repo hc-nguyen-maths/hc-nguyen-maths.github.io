09f3bebfcb4a3fce67f544281c6609cf9e0f3b29
hc-nguyen-maths <haichau1998@yahoo.fr>
2026-06-19 09:02:43 +0000

    Deploying to gh-pages from @ hc-nguyen-maths/hc-nguyen-maths.github.io@ae6afa28d09a6891336b5f21a14e0bb07eb939cd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,7 +51,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-fr-le-miracle-de-la-table-de-multiplication",
+        },{id: "post-fr-union-d-39-ensembles-et-somme-d-39-espaces-plus-qu-39-une-analogie-une-universalité",
+        
+          title: "[FR] Union d&#39;ensembles et somme d&#39;espaces : plus qu&#39;une analogie, une universalité",
+        
+        description: "Une confusion fréquente pour les apprenants d&#39;algèbre linéaire, mais qui cache une profondeur algébrique",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/somme-union/";
+          
+        },
+      },{id: "post-fr-le-miracle-de-la-table-de-multiplication",
         
           title: "[FR] Le miracle de la table de multiplication",
         
@@ -60,6 +71,17 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2026/multiplication/";
+          
+        },
+      },{id: "post-fr-la-trace-des-endomorphismes",
+        
+          title: "[FR] La trace des endomorphismes",
+        
+        description: "Le programme de MPSI introduit le déterminant d&#39;un endomorphisme par une propriété indépendante du choix de base. L&#39;objectif de ce billet est de l&#39;obtenir pour la trace.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/trace/";
           
         },
       },{id: "post-fr-espaces-de-configuration-physique-musique-et-toblerone",
